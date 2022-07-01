420050a944f084905a80e51ffc01d724a819921e
Martin Schenk <mschenk.pda@gmail.com>
2022-07-01 21:08:59 +0200

    255 auf 191 varchar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', length: 191);
+            $table->string('email', length: 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', length: 191);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email', length: 191)->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
