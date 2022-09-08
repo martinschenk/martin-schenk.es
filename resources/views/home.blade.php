45fcb4d92eaba5f8816680501f8ed004dc1dcb69
Martin Schenk <mschenk.pda@gmail.com>
2022-09-08 20:10:14 +0200

    martin schenk es main landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,8 +29,24 @@
         max-w-md mx-auto md:max-w-2xl
         px-8 py-4  ">
 
+            <h2 class="h2-normal" >Servicios</h2>
+            <p class="p-normal" >
+                Desarrollo de aplicaciónes web.
+            </p>
+            <p class="p-normal" >
+            <ul class="ul-normal"  class="ul-normal" >
+                <li class="li-normal" >
+                    <a class="a-normal" href="https://programador-php.martin-schenk.es/">programador-php.martin-schenk.es</a>
+                </li>
+                <li class="li-normal" >
+                    <a class="a-normal" href="https://desarrollador-aplicaciones-web.martin-schenk.es">desarrollador-aplicaciones-web.martin-schenk.es</a>
+                </li>
+            </ul>
+            </p>
 
-            <h2 class="h2-normal" >Productos</h2>
+
+
+            <h2 class="h2-normal" >Propias Aplicaciónes Web</h2>
 
             <p class="p-normal" >
                 <ul class="ul-normal"  class="ul-normal" >
@@ -40,15 +56,11 @@
                 </ul>
             </p>
 
-            <h2 class="h2-normal" >Servicios</h2>
-
             <p class="p-normal" >
-                <ul class="ul-normal"  class="ul-normal" >
-                    <li class="li-normal" >
-                        Herramienta para crear páginas web: <a class="a-normal" href="https://www.palimpalem.com">www.palimpalem.com</a>
-                    </li>
-                </ul>
-             </p>
+                Palimpalem es un constructor de sitios web en línea que se ha utilizado para crear más de 1.400.000 sitios web. En los mejores tiempos, se creaban unas 500 páginas web nuevas con Palimpalem cada día. Actualmente estoy trabajando en una nueva y versión que va a salir con otro dominio diferente.
+            </p>
+
+
 
         </div>
     </section>
