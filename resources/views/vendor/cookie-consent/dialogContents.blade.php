f913385b649ca25348eec925a516b9190db6f147
Martin Schenk <mschenk.pda@gmail.com>
2022-09-09 18:59:35 +0200

    cookie banner angepasst

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,14 +1,14 @@
-<div class="js-cookie-consent cookie-consent xfixed xbottom-0 xinset-x-0 pb-2">
+<div class="js-cookie-consent cookie-consent fixed bottom-0 inset-x-0 pb-4">
     <div class="max-w-7xl mx-auto px-6">
-        <div class="p-2 rounded-lg bg-yellow-100">
+        <div class="p-2 rounded-lg bg-gruen">
             <div class="flex items-center justify-between flex-wrap">
-                <div class="w-0 flex-1 items-center hidden md:inline">
-                    <p class="ml-3 text-black cookie-consent__message">
+                <div class="w-0 flex-1 items-center xhidden xmd:inline">
+                    <p class="ml-3 mr-3 xtext-lg text-gray-900 cookie-consent__message">
                         {!! trans('cookie-consent::texts.message') !!}
                     </p>
                 </div>
-                <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
-                    <button class="js-cookie-consent-agree cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-yellow-800 bg-yellow-400 hover:bg-yellow-300">
+                <div class="mt-2 flex-shrink-0 w-full mt-0 w-auto">
+                    <button class="js-cookie-consent-agree cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-3 rounded-md text-sm font-medium text-white bg-dunkelgruen ">
                         {{ trans('cookie-consent::texts.agree') }}
                     </button>
                 </div>
