3c9e650de9d045319a0c86c660bbd960efa91376
Martin Schenk <mschenk.pda@gmail.com>
2022-09-27 18:52:55 +0200

    meta og links inserted (if url is pasted)

--- resources/views/components/legal.blade.php
+++ resources/views/components/legal.blade.php
@@ -4,7 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="En Martin Schenk te ayudamos a simplificar los procesos de tu negocio con los siguientes servicios informáticos: Desarrollo de software a medida y SaaS, Hosting/alojamiento de sus aplicaciónes y webs, Mantenimiento y adaptación, Alquiler de servidores VPS y dedicados, Consultoría SaaS y aplicaciones web.">
+    <meta name="description" content="En Martin Schenk te ayudamos a simplificar los procesos de tu negocio con los nuestros servicios informáticos: Desarrollo de software a medida y SaaS, Hosting/alojamiento de sus aplicaciónes y webs, Mantenimiento y adaptación, Alquiler de servidores VPS y dedicados, Consultoría SaaS y aplicaciones web.">
+    <meta property="og:title"
+          content="Simplifica tu negocio. Martin Schenk S.L., Madrid, España.">
+    <meta property="og:description"
+          content="En Martin Schenk te ayudamos a simplificar los procesos de tu negocio">
+    <meta property="og:image" content="{{env('APP_URL')}}/img/logo-martin-schenk-weiss.svg">
+    <meta property="og:image:width" content="1200">
 
     <title class="">{{ $title ?? 'Aviso Legal' }}  {{ config('app.name', 'Laravel') }}</title>
 
