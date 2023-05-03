cd4f1a28d77f223cd42652dac720ca20619d5cb3
Martin Schenk <mschenk.pda@gmail.com>
2023-05-03 19:01:59 +0200

    schenk logo mit since

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,7 +7,7 @@
     <section class="bg-hellblau bg-cover pt-5 pl-5 pr-5 pb-2 bp-0 w-full" >
         <div class="flex items-start">
             <a href="/">
-                <img class="w-24 md:w-32 lg:w-40 xmb-2" src="/img/logo-martin-schenk-dunkel.svg" alt="Martin Schenk S.L.">
+                <img class="w-28 md:w-36 lg:w-44 " src="/img/logo-schenk-1999-dunkel.svg" alt="Martin Schenk S.L. desde 1999">
             </a>
 
         </div>
