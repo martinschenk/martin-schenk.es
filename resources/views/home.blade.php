8b9b9ba36b4153f034ac4ecdf505a4364d20bde5
Martin Schenk <mschenk.pda@gmail.com>
2023-09-18 17:59:10 +0200

    neuer peppiger text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                 <p class="md:text-3xl text-2xl text-['#1A3336'] font-black mt-6">
             <span class="" style="z-index: 1;position: relative;">
                 <img style="margin-left: 56%; width: 50%; position: absolute; top:-10px; left: 0; z-index: -1; vertical-align: middle; max-width: 100%;" src="img/texto-circulo-big.svg" alt="" width="" loading="lazy">
-                {{ __('Software personalizado que impulsa tu negocio.') }}
+                {{ __('Software a tu medida, creado con maestría.') }}
             </span>
                 </p>
             @endslot
