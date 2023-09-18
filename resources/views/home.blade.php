36deac203d9f8d5d19347f0169dcb16620c5d08b
Martin Schenk <mschenk.pda@gmail.com>
2023-09-18 17:56:28 +0200

    neuer peppiger text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
     <x-slot:title></x-slot>
 
         @php
-            $title1 = __('Desarrollo de software a medida');
-            $title2 = __('Hosting/alojamiento, dominios, cuentas email');
+            $title1 = __('Desarrollo de software personalizado');
+            $title2 = __('Hosting y dominios');
             $title3 = __('Mantenimiento');
             $title4 = __('Alquiler de servidores');
             $title5 = __('Consultoría tecnológica');
@@ -12,17 +12,17 @@
         @component('components.header')
             @slot('headertitle')
                 <p class="md:text-3xl text-2xl text-['#1A3336'] font-black mt-6">
-                <span class="" style="z-index: 1;position: relative;">
-                    <img style="margin-left: 56%; width: 50%; position: absolute; top:-10px; left: 0; z-index: -1; vertical-align: middle; max-width: 100%;" src="img/texto-circulo-big.svg" alt="" width="" loading="lazy">
-                     {{ __('Software a medida, que funciona.') }}
-                </span>
+            <span class="" style="z-index: 1;position: relative;">
+                <img style="margin-left: 56%; width: 50%; position: absolute; top:-10px; left: 0; z-index: -1; vertical-align: middle; max-width: 100%;" src="img/texto-circulo-big.svg" alt="" width="" loading="lazy">
+                {{ __('Software personalizado que impulsa tu negocio.') }}
+            </span>
                 </p>
             @endslot
         @endcomponent
 
         <section class="">
             <div class="font-sans antialiased max-w-md mx-auto md:max-w-2xl px-8 py-4">
-                <p>{{ __('En mi empresa, Martin Schenk, estoy aquí para ayudarte a simplificar los procesos de tu negocio con los siguientes servicios informáticos:') }}</p>
+                <p>{{ __('En Martin Schenk, estoy aquí para impulsar tu negocio hacia el futuro con soluciones de software personalizado.') }}</p>
                 <ul>
                     <li><a href="#{{ Str::slug($title1, '-') }}" class="text-[#26CD9C] hover:underline">{{$title1}}</a></li>
                     <li><a href="#{{ Str::slug($title2, '-') }}" class="text-[#26CD9C] hover:underline">{{$title2}}</a></li>
@@ -32,92 +32,67 @@
                 </ul>
                 <p>{{ __('Hablo Español, Alemán e Inglés.') }}</p>
 
-
                 <h2 class="" id="{{ Str::slug($title1, '-') }}">{{ $title1 }}</h2>
-
                 <p>
                 <ul>
-                    <li>{{ __('Me dedico al desarrollo y mantenimiento de aplicaciones web y SaaS, acompañándote en cada etapa:') }}
+                    <li>{{ __('Tu proyecto, desde la idea hasta la ejecución, respaldado por más de 30 años de experiencia:') }}
                         <ul class="ml-6 list-inside list-decimal">
-                            <li class="ml-6 mb-1 mt-3">{{ __('Planificación') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Diseño') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Desarrollo') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Lanzamiento') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Incorporación de nuevos módulos') }}</li>
+                            <li class="ml-6 mb-1 mt-3">{{ __('Planificación estratégica') }}</li>
+                            <li class="ml-6 mb-1">{{ __('Diseño integral') }}</li>
+                            <li class="ml-6 mb-1">{{ __('Desarrollo a medida') }}</li>
+                            <li class="ml-6 mb-1">{{ __('Lanzamiento exitoso') }}</li>
+                            <li class="ml-6 mb-1">{{ __('Expansión con módulos adicionales') }}</li>
                         </ul>
                     </li>
-                    <li>{{ __('Cada empresa es única y tiene requisitos específicos que no siempre pueden ser satisfechos por soluciones estándar, especialmente para PYMEs y corporaciones más grandes.') }}
-                    </li>
-                    <li>{{ __('Frecuentemente, desarrollar software personalizado puede ser más accesible y ajustado a las necesidades del cliente que adaptar un sistema preexistente.') }}
-                    </li>
-                    <li>{{ __('Con más de 30 años de experiencia, convierto ideas en aplicaciones web estables y fiables. SAAS es mi pasión.') }}
-                    </li>
+                    <li>{{ __('Soy tu aliado estratégico en la creación de soluciones únicas que se ajustan a las necesidades específicas de tu negocio, evitando las limitaciones de los sistemas estándar.') }}</li>
+                    <!-- (Considera agregar ejemplos específicos de cómo has ayudado a otras empresas a alcanzar sus metas a través de soluciones personalizadas) -->
                 </ul>
                 </p>
 
-                <h2 id="{{ Str::slug($title2, '-') }}">{{ $title2 }}</h2>
-                <ul>
-                    <li>{{ __('La mayoría de las aplicaciones que desarrollo se alojan y mantienen en servidores en la nube propios, liberando a los clientes de la carga del mantenimiento técnico.') }}
-                    </li>
-                    <li>{{ __('La externalización de servicios de TI es una estrategia eficiente y rentable consolidada en el sector. Facilito este proceso, encargándome del mantenimiento y actualizaciones, para que puedas enfocarte plenamente en tus metas empresariales. Estoy a una llamada de distancia para ofrecer soporte constante. Contáctame para descubrir cómo puedo ayudarte a optimizar tiempo y recursos.') }}
-                    </li>
-                </ul>
 
-                <h2 id="{{ Str::slug($title3, '-') }}">{{ $title3 }}</h2>
+                <h2 class="" id="{{ Str::slug($title2, '-') }}">{{ $title2 }}</h2>
                 <p>
                 <ul>
-                    <li>{{ __('En el mundo del software, sabemos que la adaptación y actualización constante es fundamental. Esta puede ser motivada por diversos factores como cambios en el mercado, competencia, o modificaciones estratégicas.') }}
-                    </li>
-                    <li>{{ __('Ofrezco soluciones personalizadas para el mantenimiento y actualización de tus programas, garantizando seguridad y rendimiento óptimo, y evitando obsolescencia.') }}
-                    </li>
+                    <li>{{ __('Tu dominio es tu dirección única en internet, te ayudo a seleccionar el nombre perfecto para tu negocio.') }}</li>
+                    <li>{{ __('Hosting rápido y seguro, con garantía de tiempo de actividad del 99.9%, para que tu sitio esté siempre disponible.') }}</li>
+                    <!-- (Mencionar aquí otros beneficios clave del servicio de hosting y dominios que ofreces, como el soporte técnico, backups regulares, etc.) -->
                 </ul>
                 </p>
 
-                <h2 id="{{ Str::slug($title4, '-') }}">{{ $title4 }}</h2>
+                <h2 class="" id="{{ Str::slug($title3, '-') }}">{{ $title3 }}</h2>
                 <p>
                 <ul>
-                    <li>{{ __('Gestiono aplicaciones web SaaS y sé exactamente qué tipo de servidor necesitas, pudiendo adaptar los recursos según la demanda del proyecto.') }}
-                    </li>
-                    <li>{{ __('Selecciono el VPS o servidor dedicado que mejor se ajuste a tus necesidades.') }}</li>
-                    <li>{{ __('Ofrezco la opción de mantenimiento completo.') }}</li>
-                    <li>{{ __('Llevo casi 20 años trabajando con el panel Plesk, conociéndolo a fondo.') }}</li>
-                    <li>{{ __('Realizo traslados gratuitos desde tu actual empresa de hosting hacia mis servidores.') }}</li>
+                    <li>{{ __('Asegura el óptimo funcionamiento de tu sitio web con un mantenimiento regular.') }}</li>
+                    <li>{{ __('Actualizaciones regulares para garantizar la seguridad y el rendimiento de tu sitio.') }}</li>
+                    <!-- (Detalla aquí otros servicios incluidos en el mantenimiento, como la optimización del SEO, supervisión de la salud del sitio, etc.) -->
                 </ul>
                 </p>
 
-                <h2 id="{{ Str::slug($title5, '-') }}">{{ $title5 }}</h2>
-                <p>
-                    {{ __('En Martin Schenk SL trabajo de manera estrecha con mis clientes para crear exitosos productos SaaS y  aplicaciones web que respondan a sus desafíos empresariales y necesidades particulares. Brindo asesoramiento sobre el  inconvenientes técnicos ni problemas de arquitectura. Me enorgullece poder ofrecer soluciones  tecnológicas de vanguardia que satisfacen las demandas únicas de cada empresa, siempre cumpliendo con las normativas del sector.') }}
-                </p>
-
-                <h2>{{ __('Experiencia') }}</h2>
-                <p>
-                    {{ __('Con una trayectoria de más de 20 años desarrollando, alojando y manteniendo aplicaciones web, tengo un profundo entendimiento de lo que realmente importa. Tengo una clientela que abarca desde España hasta varios países de Europa, incluyendo empresas familiares, medianas y grupos corporativos grandes.') }}
-                </p>
-
-
-                <h2>{{ __('Propias Aplicaciónes Web') }}</h2>
-
+                <h2 class="" id="{{ Str::slug($title4, '-') }}">{{ $title4 }}</h2>
                 <p>
                 <ul>
-                    <li>
-                        {{ __('Herramienta para crear páginas web:') }} <a href="https://www.palimpalem.com" target="_blank">www.palimpalem.com</a>
-                    </li>
+                    <li>{{ __('Alquila servidores de alta calidad para un rendimiento sin igual.') }}</li>
+                    <li>{{ __('Soporte técnico disponible para garantizar el máximo tiempo de actividad.') }}</li>
+                    <!-- (Podrías incluir información adicional sobre las especificaciones técnicas de los servidores y las ventajas competitivas que ofreces aquí.) -->
                 </ul>
                 </p>
 
+                <h2 class="" id="{{ Str::slug($title5, '-') }}">{{ $title5 }}</h2>
                 <p>
-                    {{ __('Palimpalem es un constructor de sitios web en línea que se ha utilizado para crear más de 1.400.000 sitios web. Se han generado hasta 500 nuevas páginas web cada día. Actualmente, estamos trabajando en una nueva versión que va a salir bajo otra marca.') }}
+                <ul>
+                    <li>{{ __('Asesoramiento experto para tomar las decisiones tecnológicas correctas para tu negocio.') }}</li>
+                    <li>{{ __('Transforma tu negocio con soluciones tecnológicas avanzadas y personalizadas.') }}</li>
+                    <!-- (Mencionar aquí cómo tu experiencia puede ayudar a las empresas a evitar trampas comunes y a elegir las soluciones más efectivas.) -->
+                </ul>
                 </p>
-
-
             </div>
         </section>
 
-
-        <x-footer></x-footer>
-
-
+    @component('components.footer')
+        @slot('footerText')
+            {{ __('© 2023 Martin Schenk - Todos los derechos reservados') }}
+        @endslot
+    @endcomponent
 </x-legal>
 
 
