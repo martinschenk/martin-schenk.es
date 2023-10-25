2be08888add851e4a0bf0b6f952a010ed1190462
Martin Schenk <mschenk.pda@gmail.com>
2023-10-25 18:51:05 +0200

    text size header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,13 +6,11 @@
             <img class="w-28 md:w-34 lg:w-40 mt-3 ml-5" src="{{ asset('/img/logo-schenk-dunkel-transp-1999-2023v3.svg') }}" alt="{{ __('Martin Schenk S.L. desde 1999') }}">
         </a>
 
-        <div class="text-center  ">
-            <h1 class="  mt-1 mb-0 text-gray-100">{{ $headertitle }}</h1>
+        <div class="text-center">
+            <h1 class="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mt-1 mb-0 text-gray-100 ">
+                {{ $headertitle }}
+            </h1>
         </div>
     </section>
-
-    {{--<section>
-        <img class="w-full h-6 object-fill object-center" src="{{ asset('/img/bottom-rund-hellblau.png') }}">
-    </section>--}}
 
 </header>
