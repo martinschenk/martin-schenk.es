b7672f373d6214775c0bde6951395fac0656ffee
Martin Schenk <mschenk.pda@gmail.com>
2023-10-26 15:06:05 +0200

    textos actualizados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,45 +11,92 @@
         <x-header headertitle="{{ __('Aplicaciones Web de Alto Impacto: Transformamos Ideas en Realidad') }}"/>
 
         <section class="font-sans antialiased max-w-md mx-auto md:max-w-2xl px-8 py-4 leading-tight">
-            <div class="mt-6">
-                <p>{{ __('Con más de tres décadas de experiencia profesional, mi enfoque se centra en la creación de soluciones web de alta complejidad. Desde bases de datos de conocimiento impulsadas por inteligencia artificial, hasta sistemas de gestión de reservas, ofrezco innovación y eficiencia en cada proyecto.') }}</p>
+            <div class="mt-8">
+                <p>{{ __('Con una trayectoria que abarca más de dos décadas, especializo en diseñar y desarrollar soluciones web de alta complejidad y impacto. Desde sistemas alimentados por inteligencia artificial como bases de datos de conocimiento, hasta aplicaciones especializadas como sistemas de gestión de reservas, cada proyecto que emprendo es un reflejo de mi compromiso con la innovación y la eficiencia."') }}</p>
 
-                <!-- Proyectos de Referencia -->
-                <h2>{{ __('Proyectos de Referencia') }}</h2>
-                <p>{{ __('Con más de 1.4 millones de páginas web creadas en Palimpalem.com, y proyectos de IA como ChatKraft, mi portafolio es una prueba de la versatilidad y robustez de mis soluciones.') }}</p>
+                <!-- Proyectos Representativos -->
+                <h2>{{ __('Proyectos Representativos') }}</h2>
+                <div class="container mx-auto px-4">
+                    <div
+                        class="text-xl font-semibold mb-4">{{ __('Mi experiencia en el campo del desarrollo de software es diversa y orientada a las necesidades individuales de cada cliente. Algunos proyectos destacados incluyen:') }}</div>
+
+                    <ul class="list-disc pl-8">
+
+                        <li class="mb-2">
+                            <strong>{{ __('Plataforma para la Creación de Páginas Web (Website Builder):') }}</strong>
+                            {{ __('Con más de 1.4 millones de páginas creadas, Palimpalem fue mi primer gran proyecto propio y demuestra la robustez y versatilidad de mis soluciones. Lanzado entre 2004-2005, el sistema experimentó un crecimiento explosivo en sus primeros años, alcanzando un promedio de 500 altas diarias. La gran desafío fue escalar el sistema para soportar esta demanda. Genera ingresos a través de publicidad de Google Adsense y otros parecidos, en páginas de usuarios gratuitos, mientras que los clientes que pagan disfrutan de funcionalidades adicionales como la ausencia de publicidad, capacidad para más páginas y más almacenamiento para multimedia. También ofrece integración de dominios y herramientas de SEO. Aunque el sistema necesita una actualización completa, todavía cuenta con una base de usuarios leales que han alojado sus páginas web durante casi dos décadas. Actualmente, lo sigo manteniendo aunque ha estado muchos años sin actualizaciones.') }}
+                        </li>
+
+
+                        <li class="mb-2">
+                            <strong>{{ __('ChatKraft - Inteligencia Artificial en Acción:') }}</strong>
+                            {{ __('Este no es simplemente un chatbot, sino una base de conocimiento impulsada por IA que automatiza y optimiza la experiencia del cliente y las operaciones internas. Utiliza algoritmos de aprendizaje profundo para comprender y responder a las consultas en tiempo real, ofreciendo así respuestas más precisas y soluciones más efectivas. ChatKraft también se integra de manera transparente con diversas bases de datos y sistemas de back-end, haciendo que la implementación sea sencilla y efectiva.') }}
+                        </li>
+
+                        <li class="mb-2">
+                            <strong>{{ __('Plataformas de Emparejamiento Laboral para Interim Managers:') }}</strong>
+                            {{ __('Este sistema avanzado funciona como un puente entre oportunidades laborales y Interim Managers altamente calificados. Los Interim Managers llenan un formulario detallado que abarca tanto su experiencia profesional como aspectos psicológicos. Utilizando un algoritmo de comparación sofisticado, el sistema escanea continuamente la base de datos de ofertas de empleo y compara los perfiles de los Interim Managers con las vacantes disponibles. Mediante un sistema de puntuación multifacético, el algoritmo genera listas de los candidatos que mejor coinciden con cada oferta de empleo. El núcleo del sistema radica en su capacidad para realizar comparaciones profundas y precisas, ofreciendo así emparejamientos altamente optimizados entre los Interim Managers y las oportunidades laborales.') }}
+                        </li>
+
+                        <li class="mb-2">
+                            <strong>{{ __('Herramientas de Valoración de Marcas para Multinacionales:') }}</strong>
+                            {{ __('Diseñado para las grandes firmas de auditoría (Wirtschaftsprüfungsgesellschaften) del mundo, este sistema SaaS resuelve el complejo desafío de valorar marcas de gran envergadura, como aerolíneas o cerveceras, para sus balances contables. El sistema proporciona una base de datos exhaustiva que recoge valoraciones de miles de marcas multinacionales. Los auditores pueden realizar búsquedas avanzadas por diversas variables como gama de producto, facturación y beneficios, y acceder a estadísticas detalladas y desviaciones estándar para marcas similares. Estas métricas estadísticas y comparativas facilitan el proceso de valoración y a menudo se incorporan en los informes oficiales para respaldar la tasación. Creado y mantenido desde cero alrededor del año 2020, el sistema ha añadido módulos nuevos y ha sido alojado y mantenido por mí durante sus primeros cuatro años de existencia.') }}
+                        </li>
+
+
+                        <li class="mb-2">
+                            <strong>{{ __('Sistemas de Reservas:') }}</strong>
+                            {{ __('Desarrollo de sistemas de reserva personalizados para gimnasios y escape rooms.') }}
+                        </li>
+
+                        <li class="mb-2">
+                            <strong>{{ __('Tienda Online para Fábrica de Frutas Deshidratadas:') }}</strong>
+                            {{ __('Diseñada para maximizar la eficiencia en ventas y administración, esta tienda online construida con Shopify ofrece una solución completa para una fábrica de frutas deshidratadas. El proyecto abarca desde el proceso de ventas hasta la administración de pedidos, incluyendo estrategias de marketing integradas para impulsar el crecimiento.') }}
+                        </li>
+
+                        <li class="mb-2">
+                            <strong>{{ __('Mercado para Periodistas Freelance:') }}</strong>
+                            {{ __('Una aplicación que permite a los periodistas vender sus reportajes y artículos directamente a medios de comunicación a través de un sistema de pujas.') }}
+                        </li>
+                    </ul>
+                </div>
+
 
                 <!-- Desarrollo de Aplicaciones Web Avanzadas -->
-                <h2 class="" id="{{ Str::slug($title1, '-') }}">{{ $title1 }}</h2>
-                <p>
-                <ul>
-                    <li>{{ __('Su visión hecha realidad en la web, con un desarrollo ágil, seguro y completamente personalizado.') }}
-                        <ul class="ml-6 list-inside list-decimal">
-                            <li class="ml-6 mb-1 mt-3">{{ __('Consultoría estratégica inicial') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Diseño centrado en el usuario') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Desarrollo a medida con tecnologías de vanguardia') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Lanzamiento seguro y escalable') }}</li>
-                            <li class="ml-6 mb-1">{{ __('Integración transparente con sistemas existentes') }}</li>
-                        </ul>
-                    </li>
-                </ul>
-                </p>
+                <h2 class="mt-14" id="{{ Str::slug($title1, '-') }}">{{ $title1 }}</h2>
+                <div class="container mx-auto px-4">
+                    <ul>
+                        <li>{{ __('Su visión hecha realidad en la web, con un desarrollo ágil, seguro y completamente personalizado.') }}
+                            <ul class="ml-6 list-inside list-decimal">
+                                <li class="ml-6 mb-1 mt-3">{{ __('Consultoría estratégica inicial') }}</li>
+                                <li class="ml-6 mb-1">{{ __('Diseño centrado en el usuario') }}</li>
+                                <li class="ml-6 mb-1">{{ __('Desarrollo a medida con tecnologías de vanguardia') }}</li>
+                                <li class="ml-6 mb-1">{{ __('Lanzamiento seguro y escalable') }}</li>
+                                <li class="ml-6 mb-1">{{ __('Integración transparente con sistemas existentes') }}</li>
+                            </ul>
+                        </li>
+                    </ul>
 
-                <!-- Logo and Technology Section -->
-                <h2>{{ __('Tecnologías y Herramientas') }}</h2>
-                <p>{{ __('Utilizo una variedad de tecnologías de vanguardia y lenguajes de programación para ofrecer soluciones eficientes y personalizadas.') }}</p>
-                <div class="flex flex-wrap space-x-6 mt-5">
-                    <img src="{{ asset('/img/logo-laravel.svg') }}" alt="Laravel" class="w-24 mr-2">
-                    <img src="{{ asset('/img/logo-livewire.svg') }}" alt="Livewire" class="w-24 mr-2">
-                    <img src="{{ asset('/img/logo-alpine.svg') }}" alt="Alpine" class="w-28 mr-2">
-                    <img src="{{ asset('/img/logo-tailwind.svg') }}" alt="Tailwind" class="w-32 mr-2">
-                    <img src="{{ asset('/img/logo-php.svg') }}" alt="PHP" class="w-[70px] h-20">
-                    <div class="w-20  transform scale-115">
-                        @include('svg.logo-filament', ['color' => 'black'])
+                    <!-- Logo and Technology Section -->
+                    <h3 class="mt-8">{{ __('Tecnologías y Herramientas') }}</h3>
+                    <div class="container mx-auto px-4">
+                        <p>{{ __('Utilizo una variedad de tecnologías de vanguardia y lenguajes de programación para ofrecer soluciones eficientes y personalizadas.') }}</p>
+                        <div class="flex flex-wrap space-x-6 mt-5 ">
+                            <img src="{{ asset('/img/logo-laravel.svg') }}" alt="Laravel" class="w-24 mr-2">
+                            <img src="{{ asset('/img/logo-livewire.svg') }}" alt="Livewire" class="w-24 mr-2">
+                            <img src="{{ asset('/img/logo-alpine.svg') }}" alt="Alpine" class="w-28 mr-2">
+                            <img src="{{ asset('/img/logo-tailwind.svg') }}" alt="Tailwind" class="w-32 mr-2">
+                            <img src="{{ asset('/img/logo-php.svg') }}" alt="PHP" class="w-[70px] h-20">
+                            <div class="w-20  transform scale-115">
+                                @include('svg.logo-filament', ['color' => 'black'])
+                            </div>
+                        </div>
                     </div>
                 </div>
 
+
                 <!-- Otros Servicios Section -->
-                <h2 class="">{{ __('Otros Servicios') }}</h2>
+                <h2 class="mt-14">{{ __('Otros Servicios') }}</h2>
 
                 <!-- Hosting y dominios Section -->
                 <h3 class="" id="{{ Str::slug($title2, '-') }}">{{ $title2 }}</h3>
