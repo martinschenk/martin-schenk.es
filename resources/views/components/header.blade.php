9787ff17470b77f3b6630afb7b957ce5b95dde0e
Martin Schenk <mschenk.pda@gmail.com>
2023-10-28 20:55:13 +0200

    logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,7 +15,7 @@
 <header class="fixed top-0 left-0 w-full h-20 bg-mittelgruen z-50 flex items-center">
     {{--logo--}}
     <a href="{{ asset('/') }}" class="ml-4">
-        <img class="w-16 sm:w-20 md:w-24 lg:w-28 xl:w-40 2xl:w-52"
+        <img class="w-40 md:w-44 lg:w-48 xl:w-52 2xl:w-56"
              src="{{ asset('/img/logo-schenk-hell-transp-1999-2023v7.svg') }}"
              alt="{{ __('Martin Schenk S.L.') }}">
     </a>
