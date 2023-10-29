309f5539c6b52d3edb3200b59f23635ed76239b5
Martin Schenk <mschenk.pda@gmail.com>
2023-10-29 16:24:24 +0100

    roboter ab large 2 spaltig

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -13,31 +13,37 @@
 @endphp
 
     <!-- Header -->
-<header class="fixed top-0 left-0 w-full h-[80px] bg-mittelgruen z-50 flex items-center justify-start py-2">
+<header
+    class="fixed top-0 left-0 w-full bg-mittelgruen z-50 flex items-center justify-start h-[80px] max-h-[80px] py-2">
     <!-- Logo -->
     <a href="{{ asset('/') }}" class="ml-4">
         <img class="w-28 sm:w-28 md:w-34 lg:w-40 xl:w-44 2xl:w-48"
-             src="{{ asset('img/logo-schenk-hell-transp-1999-2023v7.svg') }}"
+             src="{{ asset('/img/logo-schenk-hell-transp-1999-2023v7.svg') }}"
              alt="{{ __('Martin Schenk S.L.') }}">
     </a>
+</header>
 
-    <!-- Header text -->
-    <div class="absolute inset-x-0 bottom-0 flex items-center justify-center mt-2">
-        <h1 class="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-white">
+<div class="flex flex-col lg:flex-row lg:items-start bg-mittelgruen mt-20 h-[600px]"> <!-- Add a fixed height here -->
+    <!-- Header Text -->
+    <div class="flex items-center justify-center h-full lg:w-1/4 lg:ml-20">
+        <h1 class="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl text-white">
             {!! $headerTitleModified !!}
         </h1>
     </div>
-</header>
 
-<!-- image under fixed header, which fills rest of screen -->
-<div class="relative h-screen mt-20" style="min-height: calc(100vh - 80px);">
-    <picture class="absolute inset-0 object-cover object-top w-full h-full">
-        <source media="(min-width: 1536px)" srcset="{{ asset('img/robot-yellow-2-1536w.jpg') }}">
-        <source media="(min-width: 1280px)" srcset="{{ asset('img/robot-yellow-2-1280w.jpg') }}">
-        <source media="(min-width: 1024px)" srcset="{{ asset('img/robot-yellow-2-1024w.jpg') }}">
-        <source media="(min-width: 768px)" srcset="{{ asset('img/robot-yellow-2-768w.jpg') }}">
-        <img src="{{ asset('img/robot-yellow-2-640w.jpg') }}"
-             class="absolute inset-0 object-cover object-top w-full h-full">  <!-- added w-full h-full here -->
-    </picture>
+    <!-- Image -->
+    <div class="relative flex-grow lg:w-3/4 h-[600px]"> <!-- Set a maximum height -->
+        <picture>
+            <source media="(min-width: 1536px)" srcset="{{ asset('img/robot-yellow-2-1536w.jpg') }}">
+            <source media="(min-width: 1280px)" srcset="{{ asset('img/robot-yellow-2-1280w.jpg') }}">
+            <source media="(min-width: 1024px)" srcset="{{ asset('img/robot-yellow-2-1024w.jpg') }}">
+            <source media="(min-width: 768px)" srcset="{{ asset('img/robot-yellow-2-768w.jpg') }}">
+            <img src="{{ asset('img/robot-yellow-2-640w.jpg') }}" class="w-full h-full object-cover object-top">
+            <!-- Use object-cover and h-full -->
+        </picture>
+    </div>
+
 </div>
+
+
 
