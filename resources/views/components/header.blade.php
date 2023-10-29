81c37129e2a162ea21adf8fad3ab39893df7cc28
Martin Schenk <mschenk.pda@gmail.com>
2023-10-29 18:14:45 +0100

    desde md hacia arriba dos columnas en header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,4 @@
-{{-- Split the `$headertitle` into an array of words. --}}
-@php
-    $words = explode(' ', $headertitle);
-
-    // Check if there are more than 4 words in the title.
-    if (count($words) > 6) {
-        // Insert a responsive line-break after the fourth word.
-        array_splice($words, 6, 0, '<span class="sm:inline md:hidden lg:hidden xl:hidden 2xl:hidden"><br></span>');
-        $headerTitleModified = implode(' ', $words);
-    } else {
-        $headerTitleModified = $headertitle;
-    }
-@endphp
-
-    <!-- Header -->
+<!-- Header -->
 <header
     class="fixed top-0 left-0 w-full bg-mittelgruen z-50 flex items-center justify-start h-[80px] max-h-[80px] py-2">
     <!-- Logo -->
@@ -23,16 +9,16 @@
     </a>
 </header>
 
-<div class="flex flex-col lg:flex-row lg:items-start bg-mittelgruen mt-20 h-[600px]"> <!-- Add a fixed height here -->
+<div class="flex flex-col md:flex-row md:items-start bg-mittelgruen mt-20 h-[600px]"> <!-- Add a fixed height here -->
     <!-- Header Text -->
-    <div class="flex items-center justify-center h-full lg:w-1/4 lg:ml-20">
+    <div class="flex items-center justify-center h-full md:w-1/4 md:ml-20">
         <h1 class="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl text-white">
-            {!! $headerTitleModified !!}
+            {!! $headertitle !!}
         </h1>
     </div>
 
     <!-- Image -->
-    <div class="relative flex-grow lg:w-3/4 h-[600px]"> <!-- Set a maximum height -->
+    <div class="relative flex-grow md:w-3/4 h-[600px]"> <!-- Set a maximum height -->
         <picture>
             <source media="(min-width: 1536px)" srcset="{{ asset('img/robot-yellow-2-1536w.jpg') }}">
             <source media="(min-width: 1280px)" srcset="{{ asset('img/robot-yellow-2-1280w.jpg') }}">
