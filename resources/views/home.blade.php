d682e8ffd208339edb3ea32f2b27774b61457eac
Martin Schenk <mschenk.pda@gmail.com>
2023-11-06 19:12:51 +0100

    heroincons svg original codes inserted

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,11 +19,11 @@
                </p>
                <div class="flex justify-center">
                   <button
-                     class="inline-flex text-white bg-mittelgruen border-0 py-2 px-6 focus:outline-none hover:bg-teal-600 rounded text-lg">
+                     class="inline-flex text-white bg-mittelgruen border-0 py-2 px-6 focus:outline-none hover:bg-teal-600 rounded  text-sm sm:text-lg">
                      {{ __('Contáctanos') }}
                   </button>
                   <a href="#proyectos-representativos"
-                     class="ml-4 inline-flex items-center justify-center text-gray-700 bg-grau border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 hover:text-gray-800 rounded text-lg no-underline">
+                     class="ml-4 inline-flex items-center justify-center text-gray-700 bg-grau border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 hover:text-gray-800 rounded  no-underline text-sm sm:text-lg">
                      {{ __('Ver Proyectos') }}
                   </a>
 
@@ -113,7 +113,13 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Más que un chatbot, es una base de conocimiento impulsada por IA que mejora la experiencia del cliente y las operaciones internas.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-chat-bubble-left-ellipsis class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"/>
+                           </svg>
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('ChatKraft - Chat with your Knowledge') }}
                         </h2>
@@ -126,7 +132,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Conexión eficiente entre profesionales y oportunidades laborales, gracias a un algoritmo de emparejamiento avanzado.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-user-group class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"/>
+                           </svg>
+
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Plataformas para Interim Managers') }}
                         </h2>
@@ -139,7 +152,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Sistemas SaaS para grandes firmas de auditoría, especializados en la valoración de marcas de envergadura.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-calculator class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"/>
+                           </svg>
+
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Herramientas de Valoración de Marcas para Multinacionales') }}
                         </h2>
@@ -152,7 +172,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Soluciones adaptadas para gimnasios, work centers, coches y escape rooms.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-calendar class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"/>
+                           </svg>
+
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Sistemas de Reservas Personalizados') }}
                         </h2>
@@ -165,7 +192,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Desarrollo completo para maximizar eficiencia en ventas y administración.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-shopping-bag class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"/>
+                           </svg>
+
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Tienda Online para una Fábrica de Frutas Deshidratadas') }}
                         </h2>
@@ -178,7 +212,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Plataforma que permite a los periodistas vender su contenido directamente a medios de comunicación.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-newspaper class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"/>
+                           </svg>
+
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Mercado para Periodistas Freelance') }}
                         </h2>
@@ -189,9 +230,16 @@
                <!-- Motor de Búsqueda para Piezas de Autos Usadas -->
                <div class="p-4 md:w-1/2 w-full">
                   <div class="h-full bg-grau p-8 rounded">
-                     <p class="leading-relaxed mb-6">{{ __('Base de datos con más de 80,000 piezas de coches, multilingüe, con interfaz fácil para subir fotos y cálculo automático de precios de envío.') }}</p>
+                     <p class="leading-relaxed mb-6">{{ __('Plataforma para piezas de coches de segunda mano, con SEO para Google, multilingüe, y con interfaz fácil para subir fotos y cálculo automático de precios de envío.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-document-magnifying-glass class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                           </svg>
+                        </div>
+
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Motor de Búsqueda para Piezas de coches de segunda mano') }}
                         </h2>
@@ -204,7 +252,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Plataforma para exámenes y certificación online de pediatras, complementada con noticias y artículos del sector.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-academic-cap class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"/>
+                           </svg>
+
+                        </div>
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Aplicación Web para la Asociación Española de Pediatría') }}
                         </h2>
@@ -217,7 +272,14 @@
                   <div class="h-full bg-grau p-8 rounded">
                      <p class="leading-relaxed mb-6">{{ __('Nuestro primer gran proyecto, un robusto y versátil constructor de páginas web, con más de 1.4 millones de páginas creadas.') }}</p>
                      <div class="inline-flex items-center">
-                        <x-heroicon-o-wrench-screwdriver class="text-mittelgruen w-12 h-12 mr-3"/>
+                        <div>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"/>
+                           </svg>
+                        </div>
+
                         <h2 class="text-gray-900 text-lg title-font font-medium">
                            {{ __('Palimpalem') }}
                         </h2>
