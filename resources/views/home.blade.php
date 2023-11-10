e2af493d00c16b84a8605b1cb19772079c2af28e
Martin Schenk <mschenk.pda@gmail.com>
2023-11-10 16:05:36 +0100

    design verbessert. besser lesbarer text. grössen h1-h4 und base font ideal glaube ich.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,10 @@
 		<section class="">
 			<div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
 				<div class="text-center lg:w-2/3 w-full">
-					<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">
+					<h1 class="mb-4 ">
 						{{ __('Transformamos Ideas en Aplicaciones Web Innovadoras') }}
 					</h1>
-					<p class="mb-8 leading-relaxed">
+					<p class="mb-8">
 						{{ __('En Martin Schenk S.L., llevamos más de 20 años convirtiendo ideas en soluciones web efectivas y personalizadas. Tu visión, convertida en realidad.') }}
 					</p>
 					<div class="flex justify-center">
