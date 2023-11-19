6857adf99bc0537100f4ed06b1bc10c28fda2db2
Martin Schenk <mschenk.pda@gmail.com>
2023-11-19 19:29:08 +0100

    svg heart in footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -107,7 +107,12 @@
 	@php
 		$currentYear = date('Y');
 	@endphp
-	<div class="text-xs text-center">
-		© 1999 - {{ $currentYear }} Martin Schenk S.L., Madrid, {{ __('España') }}
+	<div class="flex items-center justify-center text-xs">
+		<span>© 1999 - {{ $currentYear }}</span>
+		<svg width="14" height="14" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg" class="mx-1">
+			<path
+					d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+		</svg>
+		<span>Martin Schenk S.L., Madrid, {{ __('España') }}</span>
 	</div>
 </footer>
