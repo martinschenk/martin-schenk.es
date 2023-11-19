03483445662ade46b5d8cb2e914ad16856eeb5bd
Martin Schenk <mschenk.pda@gmail.com>
2023-11-19 18:37:45 +0100

    añadido en portfolio alquier de vps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -271,6 +271,29 @@
 						</div>
 					</div>
 					
+					
+					{{--Mammuts alquiler de servidores vps--}}
+					<div class="p-4 md:w-1/2 w-full">
+						<div class="h-full bg-white border-2 border-gray-300 p-8">
+							<p class=" mb-6">{{ __('Servidores VPS configurables y pagables en línea, disponibles y listos para usar en tan solo 15 minutos gracias a la configuración automática.') }}</p>
+							<div class="inline-flex items-center">
+								<div>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+									     stroke="currentColor" class="text-mittelgruen w-12 h-12 mr-3">
+										<path stroke-linecap="round" stroke-linejoin="round"
+										      d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z"/>
+									</svg>
+								
+								
+								</div>
+								<h3 class="">
+									{{ __('Plataforma de alquiler de servidores VPS') }}
+								</h3>
+							</div>
+						</div>
+					</div>
+					
+					
 					<!-- Palimpalem -->
 					<div class="p-4 md:w-1/2 w-full">
 						<div class="h-full bg-white border-2 border-gray-300 p-8">
